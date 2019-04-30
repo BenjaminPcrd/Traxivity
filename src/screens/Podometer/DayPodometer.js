3bfa1963098ab73c30227da20d2d06b38af16ac2
bepicard <benjamin.picard@etu.uca.fr>
2019-04-30 18:44:06 +0100

    animated progressCircle

--- src/screens/Podometer/DayPodometer.js
+++ src/screens/Podometer/DayPodometer.js
@@ -3,6 +3,7 @@ import { Dimensions } from 'react-native'
 import { getAuth, getDailyStepCount } from '../../api/googleFitApi'
 import { connect } from 'react-redux';
 import GoogleFit from 'react-native-google-fit'
+import ProgressCircle from 'react-native-progress-circle'
 
 import {
   Container,
@@ -13,6 +14,7 @@ class DayPodometer extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      progress: 0,
       nbSteps: 0
     }
     getAuth()
@@ -22,17 +24,36 @@ class DayPodometer extends Component {
     GoogleFit.onAuthorize((res) => {
       getDailyStepCount((error, result) => {
         this.setState({nbSteps: result})
+        let progress = 0;
+        setInterval(() => {
+          progress += 0.5
+          if (progress > ((this.state.nbSteps/this.props.goal) * 100).toFixed(0)) {
+            progress = ((this.state.nbSteps/this.props.goal) * 100).toFixed(0)
+          }
+          this.setState({ progress });
+        }, 5);
       })
     })
   }
 
   render() {
-    console.log(this.props)
-    const screenWidth = Dimensions.get('window').width
-    return (
-      <Container>
-        <Text>{this.state.nbSteps}</Text>
 
+    console.log(this.state.progress)
+    const screenWidth = Dimensions.get('window').width
+    let progressLabel = Number(this.state.progress).toFixed(0)
+    return (
+      <Container style={{alignItems: 'center', marginTop: 20}}>
+        <ProgressCircle
+          percent={Number(this.state.progress)}
+          radius={screenWidth/3}
+          borderWidth={15}
+          color="blue"
+          shadowColor="grey"
+          bgColor="white"
+        >
+          <Text style={{ fontSize: 18 }}>{this.state.nbSteps + ' steps'}</Text>
+          <Text style={{ fontSize: 18 }}>{progressLabel + '% of the goal'}</Text>
+        </ProgressCircle>
       </Container>
     );
   }
